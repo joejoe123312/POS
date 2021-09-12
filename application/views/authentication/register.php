2c70b8bdba7ea52c999cdf3f43493aba9d97596f
jhayramirez <jhayramirez014@gmail.com>
2021-09-12 18:46:03 +0800

    photos added

--- application/views/authentication/register.php
+++ application/views/authentication/register.php
@@ -1,6 +1,6 @@
 <!-- BEGIN LOGIN SECTION -->
 <section class="section-account">
-    <div class="img-backdrop" style="background-image: url('assets/img/img16.jpg')"></div>
+    <div class="img-backdrop" style="background-image: url('assets/img/img17.jpg')"></div>
     <div class="spacer"></div>
     <div class="card contain-sm style-transparent">
         <div class="card-body">
--- application/views/dashboard.php
+++
@@ -1,4 +0,0 @@
-<h1>Welcome to dashboard</h1>
-
-<?php $logout = base_url() . "Login/logout" ?>
-<a href="<?= $logout ?>"><button>Logout</button></a>
